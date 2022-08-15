a76ea4e494f454dc6bc1dccdd47d0217614db6e1
Danielle Levine <dsl44d@gmail.com>
2022-08-15 20:25:27 +0300

    structure changes, added json data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import "./App.css";
+import React from "react";
 import Home from "./Home";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <Home />;
+  const [data, setData] = useState([]);
+
+  // refactor to Axios
+  const getData = () => {
+    fetch("data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }).then(function(response) {
+      // console.log(response);
+      return response.json().then(function(myJson) {
+        console.log(myJson);
+      });
+      // add error
+      // .catch();
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="App">
+      {data && data.length > 0 && data.map((item) => <p>{item.about}</p>)}
+      <Home />
+    </div>
+  );
 }
 
 export default App;
